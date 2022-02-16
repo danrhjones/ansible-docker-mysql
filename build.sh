85b8e3c6b72569299a1150f0689ff1aa8aec55e5
Dan Jones <dan.rh.jones@gmail.com>
2022-02-16 21:54:54 +0000

    add docker compose

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ ssh-keygen -q -t rsa -N '' -f ./id_rsa <<<y 2>&1 >/dev/null
 docker build -t docker-ansible .
  docker build -t docker-target .
 # docker run --rm -it docker-ansible ansible --version
-docker run -itd -p 2026:22  docker-target
-docker run -itd -p 2027:22 -p 5001:5000 docker-ansible
+docker run -itd -p 2026:22  docker-target --hostname controller
+docker run -itd -p 2027:22 -p 5001:5000 docker-ansible --hostname target
